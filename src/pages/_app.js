cec08db2049d2edc15ce9f5049eea96039373c72
chabonhamay4so9 <93881933+chabonhamay4so9@users.noreply.github.com>
2023-08-12 01:24:30 +0700

    Add files via upload

---
+++ src/pages/_app.js
@@ -0,0 +1,53 @@
+import {Component} from "react";
+import Head from 'next/head';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  componentDidMount() {
+    window.location.replace(this.props.props.new_url);
+  }
+
+  render() {
+    return (
+        <div >
+          <Head >
+            {this.props.props.parsedMetaTags.map((tag, index) => (
+                <meta key={index} {...tag} />
+            ))}
+          </Head>
+        </div>
+    )
+  }
+}
+export default App;
+
+App.getInitialProps = async (context) => {
+  let path_origin =context.router.asPath
+  let replace_domain = process.env.url;
+  let new_url = replace_domain+path_origin;
+  const response = await fetch(new_url);
+  const postData = await response.text();
+  const metaTags = postData.match(/<meta[^>]*>/g);
+  const parsedMetaTags = metaTags.map(tag => {
+    const nameMatch = tag.match(/name=["']([^"']*)["']/);
+    const propertyMatch = tag.match(/property=["']([^"']*)["']/);
+    const contentMatch = tag.match(/content=["']([^"']*)["']/);
+
+    return {
+      name: nameMatch ? nameMatch[1] : '',
+      property: propertyMatch ? propertyMatch[1] : '',
+      content: contentMatch ? contentMatch[1] : '',
+    };
+  });
+  // Trả về dữ liệu như là props
+  return {
+    props: {
+      parsedMetaTags,
+      new_url
+    },
+  };
+}
